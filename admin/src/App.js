9ca12c94b4b257f28040bb0ba49823b9da68a5a8
Rasheed Shodiya <shodiyarasheed100@gmail.com>
2023-02-08 17:28:10 +0100

    Added more routes

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useStateContext } from './contexts/ContextProvider';
 
 import {
-  Dashboard, Delivery, Hotels, Logout, Notifications, Profile, Rooms, Settings, Stats, Users
+  Dashboard, Delivery, Hotels, Logout, New, Notifications, Profile, Rooms, Settings, SingleUser, Stats, Users
 } from './pages';
 
 import {
@@ -13,14 +13,16 @@ import { FiSettings } from 'react-icons/fi';
 
 import './App.css';
 
+// 20.4.48 ==> ^19.4.50
+
 function App() {
   const {activeMenu, currentMode, currentColor, themeSettings, setThemeSettings} = useStateContext();
 
   return (
     <div className={currentMode === 'Dark' ? 'dark' : ''}>
       <BrowserRouter>
-        <div className="flex relative dark:bg-main-dark-bg">
-          <div className="fixed bottom-4 right-4">
+        <div className="flex relative dark:bg-main-dark-bg ">
+          <div className="fixed bottom-4 right-4 z-[10000]">
             <button
               type="button"
               onClick={() => setThemeSettings(true)}
@@ -33,7 +35,7 @@ function App() {
 
           <div>
             {activeMenu ? (
-              <div className="w-64 fixed h-full dark:bg-secondary-dark-bg bg-white z-[999] shadow-xl"> {/**sidebar */}
+              <div className="w-64 fixed h-full dark:bg-secondary-dark-bg bg-white z-[10000000] shadow-xl">
                 <Sidebar />
               </div>
             ) : (
@@ -49,13 +51,35 @@ function App() {
               ? 'md:ml-64'
               : 'flex-2'}`
           }> {/**h-full*/}
-            <main>
+            <main className="overflow-hidden">
               {themeSettings && <ThemeSettings />}
 
               <Routes>
+                {/* <Route path="/">
+                  <Route index element={<Dashboard />} />
+                  <Route path="logout" index element={<Logout />} />
+                  <Route path="users">
+                    <Route index element={<Users />} />
+                    <Route path=":userId" element={<>Single</>} />
+                    <Route path="new" element={<>New</>} />
+                  </Route>
+
+
+                  <Route path="hotels" index element={<Hotels />} />
+                  <Route path="rooms" index element={<Rooms />} />
+                  <Route path="delivery" index element={<Delivery />} />
+                  <Route path="stats" index element={<Stats />} />
+                  <Route path="notifications" index element={<Notifications />} />
+                  <Route path="settings" index element={<Settings />} />
+                  <Route path="profile" index element={<Profile />} />
+                </Route> */}
+
+                
                 <Route path="/" element={<Dashboard />} />
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/users" element={<Users />} />
+                <Route path="/users/:id" element={<SingleUser />} />
+                <Route path="/users/new" element={<New />} />
                 <Route path="/hotels" element={<Hotels />} />
                 <Route path="/rooms" element={<Rooms />} />
                 <Route path="/delivery" element={<Delivery />} />
